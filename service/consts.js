05431da72b87003ae92611c2549ddbfd73119ec6
Miki <makmel.miki@gmail.com>
2019-01-19 19:06:37 +0200

    mikis commit1

---
+++ service/consts.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MLAB_URL: 'mongodb://ds161144.mlab.com:61144/boards_r_us_db',
+    DB_USER: 'db_user',
+    DB_PASS: 'db_pass1'
+}
---
+++ service/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const consts = require('./consts');
+// const Ideas = require('./models/idea'); ///////////////
+
+const { MLAB_URL, DB_USER, DB_PASS } = consts;
+const options = {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    user: DB_USER,
+    pass: DB_PASS,
+    autoReconnect: true,
+};
+
+mongoose
+    .connect(MLAB_URL, options)
+    .then(() => console.log('connected to DB'))
+    .catch(err => console.log(`connection error: ${err}`));
